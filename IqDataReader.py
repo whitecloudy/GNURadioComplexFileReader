e86c6a44628c21aff65fe0cafbaee8969549ef4d
Whitecloudy <alex9395@naver.com>
2020-07-22 13:36:40 +0900

    IqDataReader has now iteration

--- IqDataReader.py
+++ IqDataReader.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sys import getsizeof as sizeof
 
 class IqDataReader:
     def __init__(self, filename, minReadSize = 10000):
@@ -14,8 +15,25 @@ class IqDataReader:
         self.__minReadSize = minReadSize
         self.__lastReadSize = 0
         self.__totalConsumedSize = 0
+        self.__readIndexSize = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.eof():
+            raise StopIteration
+
+        if self.__readIndexSize >= len(self.__loadedData):
+           self.__loadedData += list(np.fromfile(self.__IQstream, dtype=np.complex64, count=self.__minReadSize))
+
+        returnVal = self.__loadedData[self.__readIndexSize]
+        self.__readIndexSize+=1
+        return returnVal
 
     def read(self, read_block_size, immediateConsume=True):
+        self.consume(self.__readIndexSize)
+
         if self.eof():
             raise IndexError
 
@@ -38,19 +56,29 @@ class IqDataReader:
         consumedSize = min(len(self.__loadedData), consumedSize)
         self.__loadedData = self.__loadedData[consumedSize:]
         self.__totalConsumedSize += consumedSize
+        self.__readIndexSize = 0
         return self.__totalConsumedSize
         
     def getTotalSize(self):
+        return self.getTotalByteSize()/8
+
+    def getTotalByteSize(self):
         return self.__fileSize
 
+    def getConsumedByteSize(self):
+        return self.__IQstream.tell()
+
     def getConsumedSize(self):
-        return self.__totalConsumedSize
+        return self.__totalConsumedSize + self.__readIndexSize
 
     def getRemainSize(self):
-        return self.__fileSize - self.__totalConsumedSize
+        return self.getRemainByteSize()/8
+
+    def getRemainByteSize(self):
+        return self.__fileSize - self.__IQstream.tell()
 
     def eof(self):
-        if self.getRemainSize()==0:
+        if self.getRemainByteSize()<=0:
             return True
         else:
             return False
